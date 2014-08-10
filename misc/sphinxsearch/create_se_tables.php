ccb09ca67af31af9b40ea4ed9812d4274ad45d32
ruhllatio <ruhllatio@gmail.com>
2014-08-10 16:50:54 -0700

    Create releasefiles_rt index and SQL table structure / add to create and populate scripts

--- misc/sphinxsearch/create_se_tables.php
+++ misc/sphinxsearch/create_se_tables.php
@@ -5,8 +5,10 @@ if (nZEDb_RELEASE_SEARCH_TYPE != ReleaseSearch::SPHINX) {
 	exit('Error, nZEDb_RELEASE_SEARCH_TYPE in www/settings.php must be set to SPHINX!' . PHP_EOL);
 }
 
-if (!isset($argv[1]) || !isset($argv[2]) || !is_numeric($argv[2])) {
-	exit('Argument 1 must the hostname or IP to the Sphinx searchd server, Argument 2 must be the port to the Sphinx searchd server.' . PHP_EOL);
+if (!isset($argv[1]) || !isset($argv[2]) || !is_numeric($argv[2]) || !isset($argv[3]) || !is_bool((bool)$argv[3])) {
+	exit(
+		'Argument 1 must the hostname or IP to the Sphinx searchd server, Argument 2 must be the port to the Sphinx searchd server.' . PHP_EOL .
+		'The third and final argument is true/false.  True will drop tables if they exist and false will not.' . PHP_EOL);
 }
 
 $pdo = new nzedb\db\DB();
@@ -30,8 +32,24 @@ sprintf(
 $sphinxConnection
 );
 
+$tables['releasefiles_se'] =
+sprintf(
+"CREATE TABLE releasefiles_se
+(
+	id          BIGINT UNSIGNED NOT NULL,
+	weight      INTEGER NOT NULL,
+	query       VARCHAR(3072) NOT NULL,
+	releaseid   BIGINT UNSIGNED NOT NULL,
+	name        VARCHAR(255) NOT NULL DEFAULT '',
+	INDEX(query)
+) ENGINE=SPHINX CONNECTION=\"sphinx://%s/releasefiles_rt\"",
+$sphinxConnection
+);
+
 foreach ($tables as $table => $query) {
-	$pdo->queryExec(sprintf('DROP TABLE IF EXISTS %s', $table));
+	if ($argv[3] == true) {
+		$pdo->queryExec(sprintf('DROP TABLE IF EXISTS %s', $table));
+	}
 	$pdo->queryExec($query);
 }
 
--- misc/sphinxsearch/populate_rt_indexes.php
+++ misc/sphinxsearch/populate_rt_indexes.php
@@ -5,13 +5,16 @@ if (nZEDb_RELEASE_SEARCH_TYPE != ReleaseSearch::SPHINX) {
 	exit('Error, nZEDb_RELEASE_SEARCH_TYPE in www/settings.php must be set to SPHINX!' . PHP_EOL);
 }
 
-if (!isset($argv[1]) || !in_array($argv[1], ['releases_rt'])) {
-	exit('Argument1 is the index name, releases_rt is the only supported currently.' . PHP_EOL);
+if (!isset($argv[1]) || !in_array($argv[1], ['releases_rt', 'releasefiles_rt'])) {
+	exit('Argument1 is the index name, releases_rt and releasefiles_rt are the only supported indexes currently.' . PHP_EOL);
 }
 
 switch ($argv[1]) {
 	case 'releases_rt':
 		releases_rt();
+		break;
+	case 'releasefiles_rt':
+		releasefiles_rt();
 		break;
 	default:
 		exit();
@@ -20,6 +23,8 @@ switch ($argv[1]) {
 // Bulk insert releases into sphinx RT index.
 function releases_rt()
 {
+	global $argv;
+
 	$pdo = new nzedb\db\DB();
 	$rows = $pdo->queryExec('SELECT id, guid, name, searchname, fromname FROM releases');
 
@@ -30,7 +35,7 @@ function releases_rt()
 		$string = 'REPLACE INTO releases_rt (id, guid, name, searchname, fromname) VALUES ';
 		$tempString = '';
 		$i = 0;
-		echo '[Starting to populate sphinx RT indexes with ' . $total . ' releases.] ';
+		echo '[Starting to populate sphinx RT index ' . $argv[1] . ' with ' . $total . ' releases.] ';
 		foreach ($rows as $row) {
 			$i++;
 			$tempString .= sprintf(
@@ -52,5 +57,43 @@ function releases_rt()
 		echo ' [Done.]' . PHP_EOL;
 	} else {
 		echo 'No releases in your DB or an error occurred. This will need to be resolved before you can use the search.' . PHP_EOL;
+	}
+}
+
+// Bulk insert filenames into sphinx RT index.
+function releasefiles_rt()
+{
+	global $argv;
+
+	$pdo = new nzedb\db\DB();
+	$rows = $pdo->queryExec('SELECT id, releaseid, name FROM releasefiles');
+
+	if ($rows !== false && $rows->rowCount()) {
+		$sphinx = new SphinxSearch();
+
+		$total = $rows->rowCount();
+		$string = 'REPLACE INTO releasefiles_rt (id, releaseid, name) VALUES ';
+		$tempString = '';
+		$i = 0;
+		echo '[Starting to populate sphinx RT index ' . $argv[1] . ' with ' . $total . ' filenames.] ';
+		foreach ($rows as $row) {
+			$i++;
+			$tempString .= sprintf(
+				'(%d, %d, %s),' ,
+				$row['id'],
+				$row['releaseid'],
+				$sphinx->sphinxQL->escapeString($row['name'])
+			);
+			if ($i === 1000 || $i >= $total) {
+				$sphinx->sphinxQL->queryExec($string . rtrim($tempString, ','));
+				$tempString = '';
+				$total -= $i;
+				$i = 0;
+				echo '.';
+			}
+		}
+		echo ' [Done.]' . PHP_EOL;
+	} else {
+		echo 'No filenames in your DB or an error occurred. (Is PP Additional/PAR2 checking enabled?) This will need to be resolved before you can use the search.' . PHP_EOL;
 	}
 }
